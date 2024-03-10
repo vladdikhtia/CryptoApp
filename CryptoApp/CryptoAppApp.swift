7a77facc42abeec566e41d65dc70ff3c226ee986
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-03-10 22:00:59 +0100

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Vladyslav Dikhtiaruk on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
